9d6531821809e13ac722b3219516f9d669441c89
Scott Taylor <s.taylor06@gmail.com>
2018-10-11 11:47:15 -0600

    Changes to the Contact Us Modal and updates to allow Alex to modify the Whats in a name conent - added variables for $wian and adjusted styles...

--- wp-content/themes/one-confluence/templates/scrollmagic.php
+++ wp-content/themes/one-confluence/templates/scrollmagic.php
@@ -5,6 +5,14 @@
  * Date: 4/12/18
  * Time: 11:31 AM
  */
+
+
+//What's in a Name Variables
+//$whatsInaNameTitle = get_post_meta($post->ID, '_one_front_name_title', true);
+$whatsInaNameParagraphOne = get_post_meta($post->ID, '_one_front_name_paragraph_one', true);
+$whatsInaNameParagraphTwo = get_post_meta($post->ID, '_one_front_name_paragraph_two', true);
+$whatsInaNameParagraphThree = get_post_meta($post->ID, '_one_front_name_paragraph_three', true);
+$whatsInaNameParagraphFour = get_post_meta($post->ID, '_one_front_name_paragraph_four', true);
 ?>
 
 <div id="scroll-container">
@@ -21,7 +29,9 @@
 			<div class="col-md-1"></div>
 			<div class="col-md-10 border-left border-left1">
 				<div id="border1" class="spacer s0"></div>
-				<div class="paragraph1 scroll-content">
+				<div class="paragraph1 scroll-content tween1">
+                    <?= $whatsInaNameParagraphOne; ?>
+                    <!--
 					<div id="trigger1" class="spacer s0"></div>
 					<span class="tween1">Over the years we’ve received many inquiries as to where the business name, Plumb Kendall Solutions, came from.</span>
 					<div id="fadeOut1" class="spacer s0" style="display:inline-block;"></div>
@@ -31,6 +41,7 @@
 					<div id="trigger3" class="spacer s0" style="display:inline-block;"></div>
 					<span class="tween3">We are solution oriented builders with an eye for quality, value and customer service.</span>
 					<div id="fadeOut3" class="spacer s0"></div>
+					-->
 				</div>
 			</div>
 			<div class="col-md-1"></div>
@@ -41,7 +52,9 @@
 			</div>
 			<div class="col-md-10 border-left border-left2">
 				<div id="border2" class="spacer s0"></div>
-				<div class="paragraph2 scroll-content">
+				<div class="paragraph2 scroll-content tween2">
+                    <?= $whatsInaNameParagraphTwo; ?>
+                    <!--
 					<div id="trigger4" class="spacer s0"></div>
 					<span class="tween4">The genesis for the company’s name and logo is rather simple. We love <span id="pbTrigger" class="" style="inline;">plumb bobs</span> and what they represent.</span>
 					<div id="fadeOut4" class="spacer s0" style="display:inline-block;"></div>
@@ -51,6 +64,7 @@
 					<div id="trigger6" class="spacer s0" style="display:inline-block;"></div>
 					<span class="tween6">A plumb bob is a heavy, symmetrical object and when suspended from a string provides a perfectly vertical reference line relative to the earth’s surface. It represents integrity, truth and sustainability.</span>
 					<div id="fadeOut6" class="spacer s0"></div>
+					-->
 				</div>
 			</div>
 			<div class="col-md-1"></div>
@@ -59,7 +73,9 @@
 			<div class="col-md-1"></div>
 			<div class="col-md-10 border-left border-left3">
 				<div id="border3" class="spacer s0"></div>
-				<div class="paragraph3 scroll-content">
+				<div class="paragraph3 scroll-content tween3">
+                    <?= $whatsInaNameParagraphThree; ?>
+                    <!--
 					<div id="trigger7" class="spacer s0"></div>
 					<span class="tween7">In capable hands it can build the ancient pyramids, the <span id="etTrigger" class="" style="inline;">Eiffel Tower</span> and with the assistance of the stars, navigate great seas.</span>
 					<div id="fadeOut7" class="spacer s0" style="display:inline-block;"></div>
@@ -70,12 +86,42 @@
 					<div id="trigger9" class="spacer s0" style="display:inline-block;"></div>
 					<span class="tween9">Together we can explore new frontiers, build the future and positively transform where we live, work and play.</span>
 					<div id="fadeOut9" class="spacer s0"></div>
+					-->
 				</div>
 			</div>
 			<div class="col-md-1">
 
 			</div>
 		</div>
+        <?php if ($whatsInaNameParagraphFour !='') { ?>
+
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10 border-left border-left4">
+                <div id="border4" class="spacer s0"></div>
+                <div class="paragraph4 scroll-content">
+                    <?= $whatsInaNameParagraphFour; ?>
+                    <!--
+					<div id="trigger7" class="spacer s0"></div>
+					<span class="tween7">In capable hands it can build the ancient pyramids, the <span id="etTrigger" class="" style="inline;">Eiffel Tower</span> and with the assistance of the stars, navigate great seas.</span>
+					<div id="fadeOut7" class="spacer s0" style="display:inline-block;"></div>
+					<div id="trigger8" class="spacer s0" style="display:inline-block;"></div>
+					<div id="pbTrigger2" class="spacer s0" style="display:inline-block;"></div>
+					<span class="tween8" style="display:inline;">If you believe what we believe about the unlimited potential of the human spirit, we invite you to join us.</span>
+					<div id="fadeOut8" class="spacer s0" style="display:inline-block;"></div>
+					<div id="trigger9" class="spacer s0" style="display:inline-block;"></div>
+					<span class="tween9">Together we can explore new frontiers, build the future and positively transform where we live, work and play.</span>
+					<div id="fadeOut9" class="spacer s0"></div>
+					-->
+                </div>
+            </div>
+            <div class="col-md-1">
+
+            </div>
+        </div>
+
+        <?php } ?>
+
 		<div id="sketch" class="row">
 			<div class="col-md-1"></div>
 			<div class="col-md-12 pb-bg2 sketch-container">
