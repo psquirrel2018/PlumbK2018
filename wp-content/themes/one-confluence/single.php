338a5c767bce99b1d318d77e40d1fd8edb7daff4
Scott Taylor <s.taylor06@gmail.com>
2018-12-14 11:53:26 -0700

    Added conditional to check to see if gallery images existed and if not go full width (xs-12) on content

--- wp-content/themes/one-confluence/single.php
+++ wp-content/themes/one-confluence/single.php
@@ -12,6 +12,7 @@ get_header('two');
 global $post;
 //hero Variable
 $imageUrl = wp_get_attachment_url( get_post_thumbnail_id() );
+$images = get_field('gallery');
 ?>
     <div id="main-container" class="container-fluid">
 
@@ -25,35 +26,44 @@ $imageUrl = wp_get_attachment_url( get_post_thumbnail_id() );
 
         <?php while ( have_posts() ) : the_post(); ?>
 
+
         <div class="container" style="display:block; position:relative;">
                 <div class="row">
-                    <div class="col-xs-12 col-sm-8 section-title-wrapper" style="padding:15px;">
-                        <h1 class="light-version"><?= the_title(); ?></h1>
-                        <p><?= the_content(); ?></p>
-                    </div>
-                    <div class="col-xs-12 col-sm-4" style="padding:15px;">
-                        <div class="photos_gallery">
-                            <ul style="margin-bottom:60px;">
-                                <?php
-                                $images = get_field('gallery');
-                                if ($images):
-                                    foreach ($images as $image):
-                                        $url = $image['url'];
-                                        $type = $image['type'];
-                                        //$icon = $image['icon'];
-                                        ?>
-                                        <li class="projectGalleryItem">
-                                            <a class="galleryImage" rel="gallery1" href="<?php echo $url; ?>">
-                                                <img src="<?php echo $image['sizes']['gallery-thumb']; ?>" alt="<?php echo basename($image['sizes']['medium']); ?>" />
-                                            </a>
-                                        </li>
-                                    <?php
-                                    endforeach;
-                                endif;
-                                ?>
-                            </ul>
+                    <?php if (empty($images)) { ?>
+                        <div class="col-xs-12 section-title-wrapper" style="padding:15px;">
+                            <h1 class="light-version"><?= the_title(); ?></h1>
+                            <p><?= the_content(); ?></p>
                         </div>
-                    </div>
+                    <?php }
+                    else { ?>
+                        <div class="col-xs-12 col-sm-8 section-title-wrapper" style="padding:15px;">
+                            <h1 class="light-version"><?= the_title(); ?></h1>
+                            <p><?= the_content(); ?></p>
+                        </div>
+                        <div class="col-xs-12 col-sm-4" style="padding:15px;">
+                            <div class="photos_gallery">
+                                <ul style="margin-bottom:60px;">
+                                    <?php
+                                    $images = get_field('gallery');
+                                    if ($images):
+                                        foreach ($images as $image):
+                                            $url = $image['url'];
+                                            $type = $image['type'];
+                                            //$icon = $image['icon'];
+                                            ?>
+                                            <li class="projectGalleryItem">
+                                                <a class="galleryImage" rel="gallery1" href="<?php echo $url; ?>">
+                                                    <img src="<?php echo $image['sizes']['gallery-thumb']; ?>" alt="<?php echo basename($image['sizes']['medium']); ?>" />
+                                                </a>
+                                            </li>
+                                        <?php
+                                        endforeach;
+                                    endif;
+                                    ?>
+                                </ul>
+                            </div>
+                        </div>
+                    <?php } ?>
                 </div>
         </div>
         <?php endwhile;
